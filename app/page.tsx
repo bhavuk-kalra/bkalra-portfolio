4678319d9cef31955b3a3b584301f44dfdb6d59f
bhavuk kalra <134166717+bhavuk-kalra@users.noreply.github.com>
2024-04-24 17:44:11 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Bhavuk kalra's Portfolio
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I'm a Cybersecurity professional with hands-on experience in network security,
+        Incident Response, Creating security standards for startups adhering to NIST/ISO 
+        standards.
+        Currently honing my skills in cloud computong to seamlessly integrate security
+        measures into cloud infrastructure, ensuring robust protection against evolving cyber threats.`}
       </p>
       <div className="my-8">
         <BlogPosts />
